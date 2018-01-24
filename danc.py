249454e6c3b1fbb04575f16e7c041d8a22b6bb05
jayden <32663393+cakerchan@users.noreply.github.com>
2018-01-24 10:28:41 +0800

    Update danc.py

--- danc.py
+++ danc.py
@@ -10,15 +10,10 @@ xm = etree.HTML(m)
 kk = xm.xpath('//div[@class="articleBody"]')
 for kks in kk:
     html = etree.tostring(kks)
-    klss = re.sub('((?<=src=")(.*?)(?=/[^/]*"))', 'http://www.actualites-les.com/static/images/phy', html)
+    klss = re.sub('((?<=src=")(.*?)(?=/[^/]*"))', 'http://www.xxx.com/static/images/phy', html)
     datas1 = re.sub('<a.*?">', '', klss)
     data2 = re.sub('</a>', '', datas1)
     print data2
 
-#kl = re.sub('(?<=src=")(.*?)(?=/[^/]*$)','http://www.actualites-les.com/static/images/phy',url)
-
-#kls = 'http://www.actualites-les.com/static/images/phy' + kl
-#klss = re.sub('((?<=src=")(.*?)(?=/[^/]*"))','http://www.actualites-les.com/static/images/phy',url)
-#print klss
 
 
